22f81c998e1b8226b951e20ca07a2c2fb42cdc81
Simon Heiss <simonheiss87@gmail.com>
2017-11-11 17:08:35 +0100

    detail page is now mine

--- App.js
+++ App.js
@@ -3,20 +3,15 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 import {fetchGameSessions} from "./Backend/RestAdapter";
 import GameSessionListController from "./GameSession/GameSessionListController";
 import { StackNavigator } from 'react-navigation';
+import GameSessionDetailPage from "./GameSession/GameSessionDetailPage";
 
 const HomeScreen = ({ navigation }) => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
-            onPress={() => navigation.navigate('Details')}
+            onPress={() => navigation.navigate('GameSessionDetails', {user: 'mine'})}
             title="Go to details"
         />
         <GameSessionListController fetchGameSessions={fetchGameSessions} navigateToGameSession={() => navigation}/>
-    </View>
-);
-
-const DetailsScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
     </View>
 );
 
@@ -27,11 +22,8 @@ const RootNavigator = StackNavigator({
             headerTitle: 'Available Games',
         },
     },
-    Details: {
-        screen: DetailsScreen,
-        navigationOptions: {
-            headerTitle: 'Details',
-        },
+    GameSessionDetails: {
+        screen: GameSessionDetailPage,
     },
 });
 
--- GameSession/GameSessionDetailPage.js
+++ GameSession/GameSessionDetailPage.js
@@ -2,9 +2,13 @@ import React from 'react';
 import {StyleSheet, Text, View} from "react-native";
 
 export default class GameSessionDetailPage extends React.Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.user}`,
+    });
+
     render() {
         return (
-            <Text>{this.props.gameSession}</Text>
+            <Text>lololsss{this.props.gameSession}</Text>
         );
     }
 }
